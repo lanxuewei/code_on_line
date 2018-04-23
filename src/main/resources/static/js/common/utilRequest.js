f03b19de032ebfa571be175e57ea46a115d6c23f
lanxuewei <547426980@qq.com>
2018-04-23 21:04:51 +0800

    分离login界面代码与js代码

---
+++ src/main/resources/static/js/common/utilRequest.js
@@ -0,0 +1,22 @@
+var utilRequest = {
+
+    /**
+     * request method
+     * @returns {string}
+     */
+    GET : function () {
+        return "GET";
+    },
+
+    POST : function () {
+        return "post";
+    },
+
+    DELETE : function () {
+        return "DELETE";
+    },
+
+    PUT : function () {
+        return "PUT";
+    }
+}
---
+++ target/classes/static/js/common/utilRequest.js
@@ -0,0 +1,18 @@
+var utilRequest = {
+
+    GET : function () {
+        return "GET";
+    },
+
+    POST : function () {
+        return "post";
+    },
+
+    DELETE : function () {
+        return "DELETE";
+    },
+
+    PUT : function () {
+        return "PUT";
+    }
+}
