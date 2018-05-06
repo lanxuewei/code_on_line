f3da3f3b22d21ef78b7b40ac39d221c66ea08ced
lanxuewei <547426980@qq.com>
2018-05-06 20:47:56 +0800

    修改登录界面跳转问题

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -5,10 +5,15 @@ var login = {
          * login url
          * @returns {string}
          */
-        login : function () {
+        loginUrl : function () {
             return "/token"
         }
     },
+
+    /*init : function () {
+        login.login();
+        login.toRegister();
+    },*/
 
     /**
      * login 登陆
@@ -17,21 +22,23 @@ var login = {
         layui.use(['layer', 'form'], function(){
             var form = layui.form,
                 layer = layui.layer;
-            /**表单提交事件*/
-            $('#login-form').submit(function () {
+            /** 表单提交事件 */
+            var toLogin = function () {
+                //alert("login")
                 //layer.msg("test");
                 var username = $("#username").val();  //获得账号
                 var password = $("#password").val();  //密码
                 var status = $('input[name="identity"]:checked').val(); //身份
                 $.ajax({  //发起登陆请求
                     type : utilRequest.POST(),
-                    url : login.URL.login(),
+                    url : login.URL.loginUrl(),
                     data : "username=" + username + "&password=" + password + "&status=" + status,
                     success : function (msg) {
                         //alert(JSON.stringify(msg));
                         var code = msg.code;
                         //alert(status);
                         if (code === 0) {               //登陆成功,跳转到后台管理界面
+                            $.cookie('token', msg.data.token, { expires: 7 });  //将token存储到cookie 默认存储7天
                             if (msg.data.status ==0) {  //管理员身份
                                 location.href='management.html';
                             } else {                    //游客或者学生身份
@@ -46,7 +53,22 @@ var login = {
                         alert("request failure!");
                     }
                 });
-            });
+            };
+            $("#to-login-btn").click(toLogin);
         });
+    },
+
+    /**
+     * 跳转到注册界面
+     */
+    toRegister : function () {
+
+        /** 注册按钮点击事件 */
+        var clickToRegister = function () {
+            //alert("register");
+            location.href='/register.html';
+        }
+        /** 绑定注册按钮点击事件 */
+        $("#to-register-btn").click(clickToRegister);
     }
 }
